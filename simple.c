81a7e3dd5dbab9378f84d210311a06116f79fa78
Sivaranjani-Techie <sivaranjani3e@gmail.com>
2024-09-13 12:22:55 +0530

    Update simple.c

--- simple.c
+++ simple.c
@@ -14,6 +14,5 @@ int main() {
     printf("c = %d, d = %d\n", c, d);   // Print the results
 
     free(ptr);  // Always free the memory after use
-    printf("ptr[0] = %d\n", ptr[0]);       // Prints the value at ptr[0] (uninitialized memory). This causes **undefined behavior** since the memory is uninitialized, but not guaranteed to raise an error immediately in most cases.
     return 0;
 }
